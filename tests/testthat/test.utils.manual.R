bbbe38fc479639cf760b28c5362a6e10ccb82351
zappingseb <sebastian@mail-wolf.de>
2018-12-31 11:52:00 +0100

    Took out manual test cases if not tested in TRAVIS-CI

--- tests/testthat/test.utils.manual.R
+++ tests/testthat/test.utils.manual.R
@@ -11,36 +11,41 @@ test_that("test_manualCheck_file",{
 	
 	global_reporter <- get_reporter()
 	
-	my_reporter <- ListReporter$new()
+	# Just run manual tests on Travis CI
+	if(Sys.getenv("TRAVIS")=="TRUE"){
+		my_reporter <- ListReporter$new()
 	
-	set_reporter(my_reporter)
+		set_reporter(my_reporter)
 	
-	options("testmode" = TRUE)
+		options("testmode" = TRUE)
 	
-	RTest:::test_manualCheck_file(result = 
-					file.path(
-							find.package("RTest"),
-							"images/Roche_Logo.png"
-							),
-			reference=file.path(
-					find.package("RTest"),
-					"images/Roche_Logo.png"
-			),
-			xmlTestSpec=NULL,
-			add.desc = "mydesc",
-			openrecexp.exec = TRUE
-	)
+		
+		RTest:::test_manualCheck_file(result = 
+						file.path(
+								find.package("RTest"),
+								"images/Roche_Logo.png"
+								),
+				reference=file.path(
+						find.package("RTest"),
+						"images/Roche_Logo.png"
+				),
+				xmlTestSpec=NULL,
+				add.desc = "mydesc",
+				openrecexp.exec = TRUE
+		)
+		
+		my_reporter$end_reporter()
+		
+		options("testmode" = FALSE)
+		
+		set_reporter(global_reporter)
+		
+		expect_equal(
+				class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
+				"expectation_success"
+		)
+	}
 	
-	my_reporter$end_reporter()
-	
-	options("testmode" = FALSE)
-	
-	set_reporter(global_reporter)
-	
-	expect_equal(
-			class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
-			"expectation_success"
-	)
 		
 	
 })
@@ -50,36 +55,38 @@ test_that("test_manualCheck_file non existing reference",{
 	
 	global_reporter <- get_reporter()
 	
-	my_reporter <- ListReporter$new()
+	if(Sys.getenv("TRAVIS")=="TRUE"){
+		my_reporter <- ListReporter$new()
 	
-	set_reporter(my_reporter)
+		set_reporter(my_reporter)
 	
-	options("testmode" = TRUE)
+		options("testmode" = TRUE)
 	
-	RTest:::test_manualCheck_file(result = 
-					file.path(
-							find.package("RTest"),
-							"images/Roche_Logo.png"
-							),
-			reference=file.path(
-					find.package("RTest"),
-					"images/Roche_Logo_nonexist.png"
-			),
-			xmlTestSpec=NULL,
-			add.desc = "mydesc",
-			openrecexp.exec = TRUE
-	)
-	
-	my_reporter$end_reporter()
-	
-	options("testmode" = FALSE)
-	
-	set_reporter(global_reporter)
-	
-	expect_equal(
-			class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
-			"expectation_success"
-	)
+		RTest:::test_manualCheck_file(result = 
+						file.path(
+								find.package("RTest"),
+								"images/Roche_Logo.png"
+								),
+				reference=file.path(
+						find.package("RTest"),
+						"images/Roche_Logo_nonexist.png"
+				),
+				xmlTestSpec=NULL,
+				add.desc = "mydesc",
+				openrecexp.exec = TRUE
+		)
+		
+		my_reporter$end_reporter()
+		
+		options("testmode" = FALSE)
+		
+		set_reporter(global_reporter)
+		
+		expect_equal(
+				class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
+				"expectation_success"
+		)
+	}
 	
 })
 
@@ -88,36 +95,38 @@ test_that("test_manualCheck_file non existing result",{
 			
 	global_reporter <- get_reporter()
 	
-	my_reporter <- ListReporter$new()
-	
-	set_reporter(my_reporter)
-	
-	options("testmode" = TRUE)
-	
-	RTest:::test_manualCheck_file(result = 
-					file.path(
-							find.package("RTest"),
-							"images/Roche_Logo_nonexit.png"
-					),
-			reference=file.path(
-					find.package("RTest"),
-					"images/Roche_Logo.png"
-			),
-			xmlTestSpec=NULL,
-			add.desc = "mydesc",
-			openrecexp.exec = TRUE
-	)
-	
-	my_reporter$end_reporter()
-	
-	options("testmode" = FALSE)
-	
-	set_reporter(global_reporter)
-	
-	expect_equal(
-			class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
-			"expectation_success"
-	)
-	
+	# Just run manual tests on Travis CI
+	if(Sys.getenv("TRAVIS")=="TRUE"){
+		my_reporter <- ListReporter$new()
+		
+		set_reporter(my_reporter)
+		
+		options("testmode" = TRUE)
+		
+		RTest:::test_manualCheck_file(result = 
+						file.path(
+								find.package("RTest"),
+								"images/Roche_Logo_nonexit.png"
+						),
+				reference=file.path(
+						find.package("RTest"),
+						"images/Roche_Logo.png"
+				),
+				xmlTestSpec=NULL,
+				add.desc = "mydesc",
+				openrecexp.exec = TRUE
+		)
+		
+		my_reporter$end_reporter()
+		
+		options("testmode" = FALSE)
+		
+		set_reporter(global_reporter)
+		
+		expect_equal(
+				class(my_reporter$results$as_list()[[1]]$results[[1]])[1],
+				"expectation_success"
+		)
+	}	
 	
 })
